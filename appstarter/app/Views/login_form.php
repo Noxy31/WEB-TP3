f0d6576cde374a4dc5b1bc1b8079164fe30b1851
Noxy31 <nellenoar@gmail.com>
2024-04-03 17:03:25 +0200

    Changes login page

--- appstarter/app/Views/login_form.php
+++ appstarter/app/Views/login_form.php
@@ -1,8 +1,11 @@
-<h1>Liber Primus</h1>
-<form class="loginform" method="POST" action="/login">
-    <label for="login">Matricule / Identifiant</label>
-    <input class="input1" id="login" name="login" type="text" />
-    <label for="password">Mot de passe</label>
-    <input class="input2" id="password" name="password" type="password" />
-    <button type="submit" class="CB">Se connecter</button>
-</form>
+<div class="wrapper">
+    <h2> Login </h2>
+    <form class="loginform" method="POST" action="/login">
+        <label for="login">Matricule / Identifiant</label>
+        <input class="input1" id="login" name="login" type="text" />
+        <label for="password">Mot de passe</label>
+        <input class="input2" id="password" name="password" type="password" />
+        <button type="submit" class="CB">Se connecter</button>
+    </form>
+</div>
+</div>
