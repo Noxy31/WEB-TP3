9c7eeedceb0b0e83596c64ea35ef52a746bbbfc6
Noxy31 <nellenoar@gmail.com>
2024-04-03 22:02:56 +0200

    final design for login page

--- appstarter/app/Views/login_form.php
+++ appstarter/app/Views/login_form.php
@@ -1,11 +1,16 @@
 <div class="wrapper">
-    <h2> Login </h2>
+    <h2> LOGIN </h2>
     <form class="loginform" method="POST" action="/login">
-        <label for="login">Matricule / Identifiant</label>
-        <input class="input1" id="login" name="login" type="text" />
-        <label for="password">Mot de passe</label>
-        <input class="input2" id="password" name="password" type="password" />
+        <label for="login">Identifiant</label>
+        <div class="input-container">
+            <input class="input1" id="login" name="login" type="text" />
+            <span class="icon"><img src="assets/Icons/person.svg" alt="User Icon"></span>
+        </div>
+        <label class="label2" for="login">Mot de passe</label>
+        <div class="input-container">
+            <input class="input2" id="password" name="password" type="password">
+            <span class="icon"><img src="assets/Icons/lock-closed.svg" alt="Lock Icon"></span>
+        </div>
         <button type="submit" class="CB">Se connecter</button>
     </form>
-</div>
 </div>
