a57449a17a2b0d51d70a1423fc1002e3224f86eb
Noxy31 <nellenoar@gmail.com>
2024-04-12 00:22:05 +0200

    Updated templates

--- appstarter/app/Views/templates/gestionHeader.php
+++ appstarter/app/Views/templates/gestionHeader.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
+    <title>Bibliotheca Liber Primus</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/gestion.css');?>"/>
