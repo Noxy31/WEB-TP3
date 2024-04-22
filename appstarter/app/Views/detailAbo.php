10380c0880c09288bf04a5f4a4eb74bb0e7b7645
lova manantsoa <lovamanantsoa823@gmail.com>
2024-04-22 12:09:24 +0200

    WIP en cours

--- appstarter/app/Views/detailAbo.php
+++ appstarter/app/Views/detailAbo.php
@@ -3,8 +3,7 @@
         <li class="btn"><a href="<?php echo base_url('/gestion_abonnés'); ?>">Gestion des abonnés</a></li>
         <li><a href="<?php echo base_url('/gestion_livres'); ?>">Gestion des livres</a></li>
         <li><a href="<?php echo base_url('/gestion_exemplaires'); ?>">Gestion des exemplaires</a></li>
-        <li><a href="#">Gestion des emprunts</a></li>
-        <li><a href="#">Gestion des retours</a></li>
+        <li><a href="<?php echo base_url('/gestion des emprunts'); ?>">Gestion des emprunts</a></li>
         <li><a href="#">Gestion des demandes</a></li>
     </ul>
 </div>
--- appstarter/app/Views/gestionExemplaires.php
+++ appstarter/app/Views/gestionExemplaires.php
@@ -2,9 +2,7 @@
     <ul>
         <li class="btn"><a href="<?php echo base_url('/gestion_abonnés'); ?>">Gestion des abonnés</a></li>
         <li><a href="<?php echo base_url('/gestion_livres'); ?>">Gestion des livres</a></li>
-        <li><a href="<?php echo base_url('/gestion_exemplaires'); ?>">Gestion des exemplaires</a></li>
-        <li><a href="#">Gestion des emprunts</a></li>
-        <li><a href="#">Gestion des retours</a></li>
+        <li><a href="<?php echo base_url('/gestion_emprunts'); ?>">Gestion des emprunts</a></li>
         <li><a href="#">Gestion des demandes</a></li>
     </ul>
 </div>
